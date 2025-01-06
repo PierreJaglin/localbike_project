9cd9d0feeacefa00cb8be26fbc2dd8956407befb
Jaglidec <jaglidec@MacBookAir.lan>
2025-01-06 21:59:35 +0100

    Ajout de la partie Visualisation simple

--- models/intermediate/int_localbike_staffs.sql
+++ models/intermediate/int_localbike_staffs.sql
@@ -1,8 +1,6 @@
 SELECT 
     staff.*,
     CONCAT(staff.first_name,' ',staff.last_name) staff_name,
-    CONCAT(manager.first_name,' ',manager.last_name) manager_name,
     store.store_name
 FROM {{ ref('stg_localbike__staffs') }} staff   
-LEFT JOIN {{ ref('stg_localbike__staffs') }} manager ON CAST(staff.manager_id AS INTEGER) = manager.staff_id
 LEFT JOIN {{ ref('stg_localbike__stores')}} store on store.store_id = staff.store_id
